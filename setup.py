51f49ef1fa8cc94798958b19403777f8fb31abff
bdr00 <bad.32@outlook.com>
2017-04-11 17:01:30 +0300

    added link to release

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
     version=typedef.__version__,
     description=typedef.__description__,
     author='bdr00',
-    url='https://github.com/bdr00/typedef',  # use the URL to the github repo
-    download_url='https://github.com/peterldowns/mypackage/archive/0.1.tar.gz',  # I'll explain this in a second
+    url='https://github.com/bdr00/typedef',
+    download_url='https://github.com/bdr00/typedef/archive/v0.9.0.4.tar.gz',
     keywords=['typedef', 'struct', 'union', ' pack', 'unpack', 'binary'],
     classifiers=[
         'Programming Language :: Python :: 2',
