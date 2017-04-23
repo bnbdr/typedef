7baa42623826b030706e4af331ccb0cd41e6ecb5
bdr00 <bad.32@outlook.com>
2017-04-24 00:46:35 +0300

    fixed namespace pollution

--- typedef/__init__.py
+++ typedef/__init__.py
@@ -6,11 +6,12 @@ import typedef.errors
 from .primitives import *
 from .constants import Endian, Arch, F_SYNC, F_COPY
 from .json_encoder import TypeEncoder
-from .pragma import pack
+from . import pragma
 
 version = (0, 9, 0, 5)
 __version__ = '.'.join(map(str, version))
 __description__ = 'A somewhat convenient package for packing and unpacking structs, unions, and arrays using C-like syntax'
+__all__ = ['pragma', 'TypeEncoder', 'Endian', 'Arch', 'F_SYNC', 'F_COPY', 'errors', 'sizeof', 'struct', 'union', 'array', 'define', 'BYTE', 'WORD', 'DWORD', 'QWORD', 'HALF_PTR', 'PVOID', 'SIZE_T', 'SSIZE_T']
 
 
 def sizeof(container):
