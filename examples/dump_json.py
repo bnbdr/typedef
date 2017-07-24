72cfa1542fec53fc9a8e2a6dbd5a80aaa719d081
bdr00 <bad.32@outlook.com>
2017-07-24 21:25:41 +0300

    simplified JSON encoder output

--- examples/dump_json.py
+++ examples/dump_json.py
@@ -9,11 +9,15 @@ S = struct([
         (DWORD, 'build'),
         struct([
             (WORD, 'mj'),
-            (WORD, 'mn')
+            (WORD, 'mn'),
+            struct([
+                (BYTE, 'b1'),
+                (BYTE, 'b2')
+            ], 'even_inner')
         ], 'inn')
     ])
 ])
 
-s = S(b'\xff\xff\xff\xffaaaa\x01\x00\x02\x00')
+s = S(b'\xff\xff\xff\xffaaaa\x01\x00\x02\x00' + '\x00' + '\x02'+'!'*2) # two '!' for padding, main struct must be aligned to 4
 print('dumping json for: {}'.format(repr(s)))
 print(json.dumps(s, cls=TypeEncoder, indent=2))
