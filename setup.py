9cbb6382d19909dc3ab3a676fe93a54abf03a433
bnbdr <>
2018-06-05 13:54:26 +0300

    updated description, author

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from distutils.core import setup
 import typedef
 
+author='bnbdr'
 setup(
     name='typedef',
     packages=['typedef'],
     version=typedef.__version__,
     description=typedef.__description__,
-    author='bdr00',
+    author=author,
     author_email='bad.32@outlook.com',
-    url='https://github.com/bdr00/typedef',
-    download_url='https://github.com/bdr00/typedef/archive/v{}.tar.gz'.format(typedef.__version__),
+    url='https://github.com/{}/typedef',
+    download_url='https://github.com/{}/typedef/archive/v{}.tar.gz'.format(author, typedef.__version__),
     keywords=['typedef', 'struct', 'union', ' pack', 'unpack', 'binary'],
     classifiers=[
         'Programming Language :: Python :: 2',
--- typedef/__init__.py
+++ typedef/__init__.py
@@ -8,9 +8,9 @@ from typedef.api import *
 from typedef.constants import Endian, Arch, F_SYNC, F_COPY
 from typedef.primitives import *
 
-version = (0, 9, 0, 6)
+version = (0, 9, 0, 7)
 __version__ = '.'.join(map(str, version))
-__description__ = 'A somewhat convenient package for packing and unpacking structs, unions, and arrays using C-like syntax'
+__description__ = 'Conveniently handle structs, unions, and arrays'
 __all__ = ['pragma', 'Endian', 'Arch', 'F_SYNC', 'F_COPY', 'errors', 'sizeof', 'offsetof', 'struct',
            'union',
            'array', 'define', 'BYTE', 'WORD', 'DWORD', 'QWORD', 'HALF_PTR', 'PVOID', 'SIZE_T', 'SSIZE_T']
